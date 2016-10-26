908c0e8e440fa4c82b327a8d2175a843bb8dfda6
ccflack <ccflack@me.com>
2016-10-26 15:20:00 -0400

    Moved to branch: crud.

--- store/app/controllers/items_controller.rb
+++ store/app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
+    ROOT
+    GET   '/items'              => 'items#index', as: :items
+    GET   '/items/new'          => 'items#new', as: :new_item
+    POST  '/items'              => 'items#create'
+    GET   '/items/:id/edit'     => 'itesm#edit', as: :edit_item
+    PATCH '/items/:id'          => 'items#update',
+
   end
 end
--- store/config/routes.rb
+++ store/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  ROOT
+  GET   '/items'              => 'items#index', as: :items
+  GET   '/items/new'          => 'items#new', as: :new_item
+  POST  '/items'              => 'items#create'
+  GET   '/items/:id/edit'     => 'itesm#edit', as: :edit_item
+  PATCH '/items/:id'          => 'items#update', 
 end
